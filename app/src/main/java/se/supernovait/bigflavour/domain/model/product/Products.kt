1a7f397c0af848567a793fd0bcb47b18aa9b2916
Ahmed Shakir <ahmed.shakir@supernovait.se>
2026-07-13 20:04:03 +0400

    add NavigationEvent

--- app/src/main/java/se/supernovait/bigflavour/domain/model/product/Products.kt
+++
@@ -1,3 +0,0 @@
-package se.supernovait.bigflavour.domain.model.product
-
-data class Products(val items: List<ProductItem>)
---
+++ app/src/main/java/se/supernovait/bigflavour/presentation/navigation/NavigationEvent.kt
@@ -0,0 +1,13 @@
+package se.supernovait.bigflavour.presentation.navigation
+
+import se.supernovait.bigflavour.domain.model.product.ProductCategory
+
+sealed interface NavigationEvent {
+    data class NavigateToProduct(val category: ProductCategory): NavigationEvent
+    data class NavigateToProductDetail(val productId: Int): NavigationEvent
+    data object NavigateToSettings: NavigationEvent
+    data object NavigateToHelp: NavigationEvent
+    data object NavigateToCart: NavigationEvent
+    data object ToggleNavigationDrawer: NavigationEvent
+    data object CloseNavigationDrawer: NavigationEvent
+}
